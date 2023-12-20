e36591c8a0b483271b947ab95aaf9b3bc6d01588
Valeriya Evstratova <valelondonenko@yahoo.com>
2023-12-20 18:13:51 +0400

    add basename=/ to browser router

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLLIElemen
 root.render(
     // <React.StrictMode>
         <Provider store={store}>
-            <Router>
+            <Router basename="/">
                 <Routes>
                     <Route path='/*' element={<App/>}/>
                 </Routes>
